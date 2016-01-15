1abd71feb27ab935a9737108da8847ae18e148ab
Eric Lui <i-meric@hotmail.com>
2016-01-15 01:05:17 -0800

    winner function

--- index.php
+++ index.php
@@ -35,7 +35,13 @@
         $position = $_GET['board'];
         $squares = str_split($position);
         
-        
+        if(winner('x'$squares)){
+			echo 'You win.';
+		}else if (winner('o', $squares)){
+			echo 'I win.';
+		}else{
+			echo 'No winner yet';
+		}
         
         echo '<br/>';
         
@@ -43,3 +49,42 @@
         ?>
     </body>
 </html>
+
+<?php
+	function winner($token, $position){
+		$won = false;
+		if(($position[0]) == $token) &&
+			($position[1]) == $token) &&
+			($position[2]) == $token)){
+				$won = true;
+		} else if(($position[3]) == $token) &&
+			($position[4]) == $token) &&
+			($position[5]) == $token)){
+				$won = true;
+		} else if(($position[6]) == $token) &&
+			($position[7]) == $token) &&
+			($position[8]) == $token)){
+				$won = true;
+		} else if(($position[0]) == $token) &&
+			($position[3]) == $token) &&
+			($position[6]) == $token)){
+				$won = true;
+		} else if(($position[1]) == $token) &&
+			($position[4]) == $token) &&
+			($position[7]) == $token)){
+				$won = true;
+		} else if(($position[2]) == $token) &&
+			($position[5]) == $token) &&
+			($position[8]) == $token)){
+				$won = true;
+		} else if(($position[0]) == $token) &&
+			($position[4]) == $token) &&
+			($position[8]) == $token)){
+				$won = true;
+		} else if(($position[2]) == $token) &&
+			($position[4]) == $token) &&
+			($position[6]) == $token)){
+				$won = true;
+		} else 
+	}
+?>
